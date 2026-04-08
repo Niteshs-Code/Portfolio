edd44701e56556dddae061933604a55f23f1e4ea
Niteshs-Code <feasession8@gmail.com>
2026-04-08 21:47:20 +0530

    vefied

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,9 +7,10 @@ import "./globals.css";
 export const metadata = {
   title: "Nitesh Portfolio",
   description: "Full Stack Developer",
-  
+  other: {
+    "google-site-verification": "wMNSAN6beMZxE7dUCE_UV2TpP3CSBTbSXAtmVJXleqE",
+  }, 
 };
-
 
 export default function RootLayout({ children }) {
   return (
